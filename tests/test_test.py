bc4a665ff0dee051efec3373ab30b640db586ec3
Syam Pannala <thutupa@gmail.com>
2014-06-28 13:09:32 -0700

    initial test

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,26 +2,7 @@ import unittest
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import testbed
-
-class TestModel(db.Model):
-  """A model class used for testing."""
-  number = db.IntegerProperty(default=42)
-  text = db.StringProperty()
-
-class TestEntityGroupRoot(db.Model):
-  """Entity group root"""
-  pass
-
-def GetEntityViaMemcache(entity_key):
-  """Get entity from memcache if available, from datastore if not."""
-  entity = memcache.get(entity_key)
-  if entity is not None:
-    return entity
-  entity = TestModel.get(entity_key)
-  if entity is not None:
-    memcache.set(entity_key, entity)
-  return entity
-
+from redirect import Action
 
 class DemoTestCase(unittest.TestCase):
 
@@ -30,16 +11,15 @@ class DemoTestCase(unittest.TestCase):
     self.testbed = testbed.Testbed()
     # Then activate the testbed, which prepares the service stubs for use.
     self.testbed.activate()
-    # Next, declare which service stubs you want to use.
+
     self.testbed.init_datastore_v3_stub()
-    self.testbed.init_memcache_stub()
+
 
   def tearDown(self):
         self.testbed.deactivate()
 
   def testInsertEntity(self):
-        TestModel().put()
-        self.assertEqual(1, len(TestModel.all().fetch(2)))
+    Action().put()
 
 if __name__ == '__main__':
     unittest.main()
