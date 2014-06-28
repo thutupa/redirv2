73c187f66d71156fec85d08a853406107323f935
Syam Pannala <thutupa@gmail.com>
2014-06-28 16:15:46 -0700

    add test for phrase attribute

--- tests/action_test.py
+++ tests/action_test.py
@@ -25,5 +25,15 @@ class DemoTestCase(unittest.TestCase):
 
     self.assertEquals(1, len(Action.query().fetch(2)))
 
+
+  def testCanFetchAttribute(self):
+    act = Action()
+    act.phrase = 'test phrase'
+    act.put()
+
+    self.assertEquals(1, len(Action.query().fetch(2)))
+    fetched = Action.query().fetch(2)[0]
+    self.assertEquals(fetched.phrase, act.phrase)
+
 if __name__ == '__main__':
     unittest.main()
