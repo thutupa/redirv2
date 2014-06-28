e0c6bf3055b4bc2da2950e21f2b86cfa1eabe5ee
Syam Pannala <thutupa@gmail.com>
2014-06-28 08:23:16 -0700

    Initial version with a test. Not sure the test works

---
+++ redirect.py
@@ -0,0 +1,17 @@
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler): pass
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
