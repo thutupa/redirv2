c438f3ed155622d83d2542642df6717fcbe3f38f
Syam Pannala <thutupa@gmail.com>
2014-06-28 16:09:28 -0700

    refactor to remove b.py

--- tests/b.py
+++
@@ -1,5 +0,0 @@
-    sdk_path = '/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine'
-    sys.path.insert(0, sdk_path)
-    import dev_appserver
-    dev_appserver.fix_sys_path()
-    
