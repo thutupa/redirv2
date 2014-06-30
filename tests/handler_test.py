ee38640a09158b362add05ce602fd42b5006ef7f
Syam Pannala <thutupa@gmail.com>
2014-06-30 11:47:57 -0700

    use constants in test

--- tests/handler_test.py
+++ tests/handler_test.py
@@ -3,14 +3,15 @@ import webtest
 import unittest
 
 from redirect import AddHandler
+from constants import Constants
 
 class AddHandlerTest(unittest.TestCase):
     def setUp(self):
         # Create a WSGI application.
-        app = webapp2.WSGIApplication([('/add', AddHandler)])
+        app = webapp2.WSGIApplication([(Constants.Paths.ADD_PATH, AddHandler)])
         self.testapp = webtest.TestApp(app)
 
     # Test the handler.
     def testAddHandlerReturnErrorWhenCalledWithNoParams(self):
-        response = self.testapp.get('/add')
+        response = self.testapp.get(Constants.Paths.ADD_PATH)
         self.assertEqual(response.status_int, 500)
