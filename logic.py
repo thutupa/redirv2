f810cc7ff9de286a2c62fd10f6e20ef060c957ff
Syam Pannala <thutupa@gmail.com>
2014-06-30 11:26:41 -0700

    implement update action

--- logic.py
+++ logic.py
@@ -7,4 +7,8 @@ def InsertAction(userId, phrase, link):
     act.redirect_link = link
     return act.put()
 
-def UpdateAction(key, newPhrase, newLink): pass
+def UpdateAction(key, newPhrase, newLink):
+    act = key.get()
+    act.setKeywordsFromPhrase(newPhrase)
+    act.redirect_link = newLink
+    act.put()
