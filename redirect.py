13410208eb4765800542d7c3d51d055b7447640b
Syam Pannala <thutupa@gmail.com>
2014-06-30 11:45:31 -0700

    failing test for add handler

--- redirect.py
+++ redirect.py
@@ -4,14 +4,14 @@ import jinja2
 import webapp2
 import os
 
-import models.Action
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class MainPage(webapp2.RequestHandler): pass
+
+class AddHandler(webapp2.RequestHandler): pass
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
--- tests/handler_test.py
+++ tests/handler_test.py
@@ -1,23 +1,16 @@
 import webapp2
 import webtest
-
-class HelloWorldHandler(webapp2.RequestHandler):
-   def get(self):
-       # Create the handler's response "Hello World!" in plain text.
-       self.response.headers['Content-Type'] = 'text/plain'
-       self.response.out.write('Hello World!')
-
 import unittest
 
-class AppTest(unittest.TestCase):
+from redirect import AddHandler
+
+class AddHandlerTest(unittest.TestCase):
     def setUp(self):
         # Create a WSGI application.
-        app = webapp2.WSGIApplication([('/', HelloWorldHandler)])
+        app = webapp2.WSGIApplication([('/add', AddHandler)])
         self.testapp = webtest.TestApp(app)
 
     # Test the handler.
-    def testHelloWorldHandler(self):
-        response = self.testapp.get('/')
-        self.assertEqual(response.status_int, 200)
-        self.assertEqual(response.normal_body, 'Hello World!')
-        self.assertEqual(response.content_type, 'text/plain')
+    def testAddHandlerReturnErrorWhenCalledWithNoParams(self):
+        response = self.testapp.get('/add')
+        self.assertEqual(response.status_int, 500)
