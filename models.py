b80c4709eaa3f4559160175b97d0e0e4cd13b626
Syam Pannala <thutupa@gmail.com>
2014-06-29 19:17:29 -0700

    elementary split

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 from google.appengine.ext import ndb
 
+MAX_NUM_KEYWORDS = 10
 class Action(ndb.Model):
     keywords = ndb.StringProperty(repeated=True)
     redirect_link = ndb.StringProperty(indexed=False)
     
-    def setKeywordsFromPhrase(self, phrase): pass
+    def setKeywordsFromPhrase(self, phrase):
+        # extra split to ignore
+        words = phrase.split(' ', MAX_NUM_KEYWORDS + 1)
+        words = [word.lower() for word in words[:MAX_NUM_KEYWORDS]]
+        self.keywords = words
