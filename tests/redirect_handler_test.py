6620b52adad9f90a61280e40a487edfebbe841a5
Syam Pannala <thutupa@gmail.com>
2014-07-01 19:25:37 -0700

    add test for redirect existence

---
+++ tests/redirect_handler_test.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+import webapp2
+import webtest
+import unittest
+
+from redirect import AddHandler
+import redirect
+from constants import Constants
+import mock
+import logic
+
+
+class RedirectHandlerTest(unittest.TestCase):
+    def setUp(self):
+        # Create a WSGI application.
+        app = redirect.application
+        self.testapp = webtest.TestApp(app)
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_user_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    # Test something is setup to handle /add
+    def testRedirectHandlerExistsForGet(self):
+        response = self.testapp.get(Constants.Paths.REDIRECT_PATH, expect_errors=True)
+        self.assertNotEqual(response.status_int, 405)
+        self.assertNotEqual(response.status_int, 404)
