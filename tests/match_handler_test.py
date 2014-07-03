1145a07658e668846ae812a29aa9573fd042799f
Syam Pannala <thutupa@gmail.com>
2014-07-02 19:07:22 -0700

    test match handler allows post

---
+++ tests/match_handler_test.py
@@ -0,0 +1,34 @@
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+import webapp2
+import webtest
+import unittest
+import urlparse
+
+from redirect import AddHandler
+import redirect
+from constants import Constants
+import mock
+import logic
+import models
+
+class MatchHandlerTest(unittest.TestCase):
+    def setUp(self):
+        # Create a WSGI application.
+        app = redirect.application
+        self.testapp = webtest.TestApp(app)
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_user_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    # Test something is setup to handle /match
+    def testMatchHandlerExistsForPost(self):
+        response = self.testapp.post(Constants.Path.MATCH_PATH, expect_errors=True)
+        self.assertNotEqual(404, response.status_int)
+        self.assertNotEqual(405, response.status_int)
+    
+    
