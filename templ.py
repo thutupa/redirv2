4c03aa83dd5075e17f39c35311f663739bb17492
Syam Pannala <thutupa@gmail.com>
2014-07-03 00:58:40 -0700

    add templ and test

---
+++ templ.py
@@ -0,0 +1,23 @@
+import os
+import jinja2
+import constants
+
+def templateDir():
+    return os.path.join(os.path.dirname(__file__), 'templates')
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(templateDir()),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+# TODO(syam): Could this lookup be cached?
+def lookupTemplate(fname):
+    return JINJA_ENVIRONMENT.get_template(fname)
+
+def InsertJson(actionId=''):
+    templ = lookupTemplate('insert.json')
+    return _renderTemplate(templ, {'id': actionId})
+
+def _renderTemplate(templ, params):
+    params['Constants'] = constants.Constants
+    return templ.render(params)
---
+++ tests/templ_test.py
@@ -0,0 +1,11 @@
+import unittest
+import templ
+
+class RedirectHandlerTest(unittest.TestCase):
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    # Test something is setup to handle /redirect
+    def testInsertTemplateReturnsSomething(self):
+        self.assertTrue(templ.InsertJson('testId') is not None)
